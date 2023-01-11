2b857fea12461ea81ef95f1bd448d20230a5a279
ZrisanDev <103600178+ZrisanDev@users.noreply.github.com>
2023-01-11 15:42:15 -0500

    finalizado

--- index.js
+++ index.js
@@ -2,31 +2,53 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
+require("dotenv").config();
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  // console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + process.env.PORT);
+});
+
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date;
+
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    if (dateString === undefined) {
+      let dateObject = new Date();
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    } else {
+      let dateObject = new Date(dateString);
+
+      if (dateObject.toString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+      } else {
+        res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+      }
+    }
+  }
 });
